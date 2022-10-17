66823b95754f366790bfccc016f6af6f1e40c4ea
Stephen <stephen@umw.edu>
2022-10-17 12:44:51 -0400

    Create dynamic menu of flavor choices.

--- routes.py
+++ routes.py
@@ -1,10 +1,16 @@
 
+from flask import render_template
 from ben_and_jerry import maxx
+import sqlite3
 
 
 @maxx.route("/")
 def noor():
-    return "<HTML><BODY><H1>Hi there!</H1></HTML>"
+    conn = sqlite3.connect("bj.sqlite")
+    cur = conn.cursor()
+    cur.execute("select distinct flavorName from recipe")
+    our_query_results = cur.fetchall()
+    return render_template("chooseflavor.html", flavornames=our_query_results)
 
 @maxx.route("/ben")
 def ben():

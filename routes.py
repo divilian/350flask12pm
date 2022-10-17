7011c9b6cb707c119744231309f4abf8871ca3a7
Stephen <stephen@umw.edu>
2022-10-17 12:51:32 -0400

    Lie and say we like the same flave as user.

--- routes.py
+++ routes.py
@@ -1,16 +1,20 @@
 
-from flask import render_template
+from flask import render_template, request
 from ben_and_jerry import maxx
 import sqlite3
 
 
 @maxx.route("/")
 def noor():
-    conn = sqlite3.connect("bj.sqlite")
-    cur = conn.cursor()
-    cur.execute("select distinct flavorName from recipe")
-    our_query_results = cur.fetchall()
-    return render_template("chooseflavor.html", flavornames=our_query_results)
+    if "faveflave" in request.args:
+        return f"<HTML><BODY><p>Wow, {request.args['faveflave']} is my favorite too!</BODY></HTML>"
+    else:
+        conn = sqlite3.connect("bj.sqlite")
+        cur = conn.cursor()
+        cur.execute("select distinct flavorName from recipe")
+        our_query_results = cur.fetchall()
+        return render_template("chooseflavor.html",
+            flavornames=our_query_results)
 
 @maxx.route("/ben")
 def ben():

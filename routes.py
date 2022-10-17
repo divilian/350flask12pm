da1b9dc4bc56cbbdd703d5c14b73aad9803bc27f
Stephen <stephen@umw.edu>
2022-10-17 13:22:11 -0400

    Browse recipes for given flavor.

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from ben_and_jerry import maxx
 import sqlite3
 
@@ -7,16 +7,32 @@ import sqlite3
 @maxx.route("/")
 def noor():
     if "faveflave" in request.args:
-        return f"<HTML><BODY><p>Wow, {request.args['faveflave']} is my favorite too!</BODY></HTML>"
+        return redirect(url_for("browserecipes",
+            flavor=request.args['faveflave']))
     else:
         conn = sqlite3.connect("bj.sqlite")
         cur = conn.cursor()
         cur.execute("select distinct flavorName from recipe")
         our_query_results = cur.fetchall()
+        conn.close()
         return render_template("chooseflavor.html",
             flavornames=our_query_results)
 
 @maxx.route("/ben")
 def ben():
     return "<HTML><BODY><H1>Yet another file</H1></BODY></HTML>"
+
+@maxx.route("/browserecipes")
+def browserecipes():
+    conn = sqlite3.connect("bj.sqlite")
+    cur = conn.cursor()
+    cur.execute("select name,cartonsOrdered from recipe where flavorName=?",
+        (request.args['flavor'],))
+    recipestuff = cur.fetchall()
+    conn.close()
+    return render_template("browserecipes.html", flavor=request.args['flavor'],
+        recipestuff=recipestuff)
+
+
+
 
